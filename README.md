dfa0c7ce01f31321500279f99530f0b19979963e
LuCC-que <chen1968493689@gmail.com>
2024-03-16 21:25:23 +0000

    final version

---
+++ README.md
@@ -0,0 +1,8 @@
+create a file in src/ name build
+
+and then cd into that folder
+
+type cmake ..
+
+and then make
+
--- src/Prover.cpp
+++ src/Prover.cpp
@@ -54,8 +54,50 @@ void Prover::generateRandomBoolMatrix(size_t size) {
 }
 
 void Prover::generateCombinations() {
+    /*
+        Explaining (i & (1 << j)) != 0
+        the wss.size() is total the number of combinations
+        the wss[0].size() is the number of variables
+
+        for example if wss[0].size() = 3
+        then the combinations are 2^3 = 8
+
+        000
+        001
+        010
+        ...
+        111
+
+        our goal is to generate all the combinations
+        but in vector<bool> form, we can acheive this
+        by shift least significant bit of value 1 by j
+        position and then do bitwise AND with i
+
+        like this, suppose size = 3
+
+        i = 1, we want vector<bool> = {0, 0, 1}
+
+
+        i: 1, j: 0, 1 << j: 1, i & (1 << j): 1, result: 1
+        i: 1, j: 1, 1 << j: 2, i & (1 << j): 0, result: 0
+        i: 1, j: 2, 1 << j: 4, i & (1 << j): 0, result: 0
+
+        more intuitive example
+
+        1 = 001 in 3 bits
+        001 & 001 = 1
+        001 & 010 = 0
+        001 & 100 = 0
+
+        rest of the case are like this
+
+    */
     for (size_t i = 0; i < wss.size(); i++) {
         for (size_t j = 0; j < wss[0].size(); j += 4) {
+            // I don't have support for 2x2 matrix
+            // so I am assuming the size is at least 4
+            // and multiple of 4, so I can do the
+            // loop unrolling to acclelerate the process
             wss[i][j] = (i & (1 << j)) != 0;
             wss[i][j + 1] = (i & (1 << (j + 1))) != 0;
             wss[i][j + 2] = (i & (1 << (j + 2))) != 0;
@@ -66,10 +108,30 @@ void Prover::generateCombinations() {
 
 void Prover::generateCombinations(size_t n, const vector<size_t>& prevals,
                                   uint8_t count) {
+    /*
+        this function is similar to the above function
+
+        but here we have fixed values
+
+        like if we have size = 3, but prevals = {124}
+
+        then we have to generate the combinations of 0s and 1s
+        of size 3 - 1 = 2
+
+        {124, 0, 0}
+        {124, 0, 1}
+        {124, 1, 0}
+        {124, 1, 1}
+
+        and this will be the all permutations of a group
+    */
     size_t totalCombinations = 1 << (n - prevals.size());
     groupedCombinations[count].resize(totalCombinations);
     for (size_t i = 0; i < totalCombinations; ++i) {
         groupedCombinations[count][i].resize(n);
+
+        // copy the fixed values into groupedCombination
+        // to fixed the unchanged position
         copy(prevals.begin(), prevals.end(), groupedCombinations[count][i].begin());
         for (size_t j = 0; j < n - prevals.size(); ++j) {
             groupedCombinations[count][i][prevals.size() + j] = (i & (1 << j)) != 0;
@@ -79,13 +141,49 @@ void Prover::generateCombinations(size_t n, const vector<size_t>& prevals,
 
 void Prover::generateGroupedCombinations(size_t n, const vector<size_t>& rs,
                                          size_t k) {
-    // Expand A to size n
+    /*
+    We separte the random values into k groups
+
+    since we are always have three group
+
+    take 4x4 as example
+    k = log2(4)*2, n = log2(4)*3, put it simply
+    log2(4) is number of elements in a group
+    k is the number of elements in a group combination
+    n is the number of elements of three vertices
+
+    x1x2x3x4
+    x1x2x5x6
+    x3x4x5x6
+
+    xi is variable that can be 0 or 1,
+
+    thus, we can have x1x2 as G1, x3x4 as G2, x5x6 as G3
+
+    xi can be replaced by fixed value from received random values, then it will
+    not have chance to be 0 or 1, it just fixed value
+
+    k = log2(matrix.size())*2, represent how much many elements
+    in a group, in 8x8, k = 6, n = 9
+
+    x1x2x3x4x5x6
+    x1x2x3x7x8x9
+    x4x5x6x7x8x9
+
+    this works for any size of matrix, 64x64, 128x128, etc
+    but just slower for larger matrix
+
+    */
+
     uint8_t count = 0;
     uint8_t rs_pushed_count = 0;
     const size_t rs_size = rs.size();
 
     for (size_t i = 0; i < n; i += k) {
         vector<size_t> prevals{};
+
+        // push the random values into prevals as fixed value
+        //  if have position left that will be free values that can be 0 or 1
         for (; rs_pushed_count < rs_size;) {
             prevals.push_back(rs[rs_pushed_count++]);
             size_t prevals_size = prevals.size();
@@ -142,6 +240,26 @@ vector<size_t> Prover::calculate_subPolyMle() {
         // copy rs into temp vector and add des_input
         vector<size_t> temp_rs(random_values.begin(), random_values.end());
         temp_rs.push_back(des_input);
+
+        /*
+         Chek the description of generateGroupedCombinations
+
+         since we have three groups, we have to generate the combinations
+
+         take 4x4 as example
+
+         x1x2 = G1 = {all permutation of G1}
+         x3x4 = G2 = {all permutation of G2}
+         x5x6 = G3 = {all permutation of G3}
+
+         Then we can pick one possibilty from each G1, G2, G3
+         denote g1, g2, g3
+
+         then we can concanate g1g2, g1g3, g2g3 to be all the possible
+         inputs for this round, and so on
+
+
+        */
 
         generateGroupedCombinations(n, temp_rs, k);
 
